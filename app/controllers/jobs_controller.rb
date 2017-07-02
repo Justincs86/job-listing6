c3eb3331edf772facd5d559af14d38e6db6995d0
justin <justinng@Justins-MacBook-Air.local>
2017-07-02 22:14:28 +0800

    implement CRUD & jobs/index

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,51 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+      flash[:notice] = "Job created"
+      redirect_to jobs_path
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+      flash[:noitce] = "Job Updated"
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+    flash[:alert] = "Job Deleted"
+      redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+  
+
+end
